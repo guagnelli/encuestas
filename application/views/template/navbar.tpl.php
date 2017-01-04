3d508f6938a884ea63c421a739d2a94f4abd5112
leas88 <cenitluis.pumas@gmail.com>
2017-01-04 13:57:06 -0600

    Menu de encuestas contestadas no lo muestrá

--- application/views/template/navbar.tpl.php
+++ application/views/template/navbar.tpl.php
@@ -1,7 +1,19 @@
 <?php
 $logueado = $this->session->userdata('logueado');
 $nombre = $this->session->userdata("nombre");  //Tipo de usuario almacenado en sesiÃ³n
-pr($this->uri->segment(1));
+//pr($this->uri->segment(1));
+//pr($this->uri->segment(2));
+$array_controlador = array('encuestausuario' => array('lista_encuesta_usuario'));
+$valida_menu = 1;
+foreach ($array_controlador as $controlador => $metodos) {
+    if ($controlador == $this->uri->segment(1)) {
+        foreach ($metodos as $value) {
+            if ($value == $this->uri->segment(2)) {
+                $valida_menu = 0;
+            }
+        }
+    }
+}
 if (isset($logueado) && !empty($logueado)) {
     ?>
     <nav class="navbar navbar-inverse">
@@ -11,30 +23,34 @@ if (isset($logueado) && !empty($logueado)) {
                     <span class="glyphicon glyphicon-log-out"></span></a>
             </div>
             <ul class="nav navbar-nav">
-                <li class="active"><a href="<?php echo site_url('encuestas/index'); ?>">Encuestas</a></li>
-                <li><a href="<?php echo site_url('curso'); ?>" class="a_nav_sied" >Implementaciones</a>
-                </li>
-                <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">Reportes
-                        <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <!--<li><a href="<?php // echo site_url('reporte_general');  ?>" class="a_menu">Reporte general</a></li>-->
-                        <li><a href="<?php echo site_url('reporte'); ?>" class="a_menu">Reporte resumen de bonos</a></li>
-                        <li><a href="<?php echo site_url('reporte_bonos'); ?>" class="a_menu">Reporte de implementación</a></li>
-                        <li><a href="<?php echo site_url('reporte_detallado'); ?>" class="a_menu">Reporte detalle de encuestas</a></li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="<?php echo site_url('login/regresar_sied'); ?>" class="a_nav_sied">
-                        Regresar a SIED
-                    </a>
-                </li>
+                <?php if ($valida_menu) { ?>
+                    <li class="active"><a href="<?php echo site_url('encuestas/index'); ?>">Encuestas</a></li>
+                    <li><a href="<?php echo site_url('curso'); ?>" class="a_nav_sied" >Implementaciones</a>
+                    </li>
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Reportes
+                            <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <!--<li><a href="<?php // echo site_url('reporte_general');    ?>" class="a_menu">Reporte general</a></li>-->
+                            <li><a href="<?php echo site_url('reporte'); ?>" class="a_menu">Reporte resumen de bonos</a></li>
+                            <li><a href="<?php echo site_url('reporte_bonos'); ?>" class="a_menu">Reporte de implementación</a></li>
+                            <li><a href="<?php echo site_url('reporte_detallado'); ?>" class="a_menu">Reporte detalle de encuestas</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="<?php echo site_url('login/regresar_sied'); ?>" class="a_nav_sied">
+                            Regresar a SIED
+                        </a>
+                    </li>
+                <?php } ?>
             </ul>
         </div>
     </nav>
 
-    <div style="text-align:right"><small><?php echo $nombre; ?> | </small></div>
-    <div class="clearfix"></div>
+    <?php if ($valida_menu) { ?>
+        <div style="text-align:right"><small><?php echo $nombre; ?> | </small></div>
+        <div class="clearfix"></div>
+    <?php } ?>
     <?php
 } 
 
